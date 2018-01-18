fa4582a53b4f3fe2800aa0dc05626d1743522eec
student <isakovbabadchanyan@mail.ru>
2018-01-18 21:29:06 +0400

    registration

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,8 +14,8 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
-    @RequestMapping("/Hello")
- public String hello(){
-         return "Hello";
+    @RequestMapping(value = "/Register",method = RequestMethod.GET)
+ public String register(){
+         return "Register";
     }
 }
