e3da4006e2309d20d452f33361b5befba0746a8f
student <isakovbabadchanyan@mail.ru>
2018-01-23 20:49:40 +0400

    login

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
-    @RequestMapping(value = "/Register",method = RequestMethod.GET)
- public String register(){
-         return "Register";
+
+    @RequestMapping(value = "/Register", method = RequestMethod.GET)
+    public String register() {
+        return "Register";
+    }
+
+    @RequestMapping(value = "/Register", method = RequestMethod.POST)
+    public String registerpost(@RequestParam("email") String email,
+                               @RequestParam("pass")String pass,
+                               @RequestParam("pass_repeat")String pass_repeat,
+                               @RequestParam("first_name")String first_name,
+                               @RequestParam("last_name")String last_name) {
+        if (pass.equals(pass_repeat)){
+        System.out.println(email +" "+ pass +" "+ pass_repeat +" "+ first_name +" "+ last_name);
+        userService.create(new User(email,pass,first_name,last_name));
+        return "Hello";
+    }else{
+        throw new IllegalArgumentException("repeat@ vhe hamapatasxanum passin");
+    }
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String Login(){
+        return "login";
     }
 }
